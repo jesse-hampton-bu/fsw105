e96d89dbeef7614721750172aaf27540581b180b
jesse-hampton-bu <65149406+jesse-hampton-bu@users.noreply.github.com>
2020-07-19 10:06:25 -0700

    fourth-commit

---
+++ week3/loops_and_arrays.js
@@ -0,0 +1,28 @@
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+    {
+        name: "Mike",
+        age: 12,
+        gender: "male"
+    }, {
+        name: "Madeline",
+        age: 80,
+        gender: "female"
+    }, {
+        name: "cheryl",
+        age: 22,
+        gender: "female"
+    }, {
+        name: "sam",
+        age: 30,
+        gender: "male"
+    }, {
+        name: "suzy",
+        age: 4,
+        gender: "female"
+    }
+]
+
+var peopleWhoWantToSeeMadMaxFuryRoad = ["Mike", "Madeline", "Cheryl", "Sam", "Suzy"]
+
+console.log((peopleWhoWantToSeeMadMaxFuryRoad.age >= 18) ? "old enough" : "not old enough");
+
