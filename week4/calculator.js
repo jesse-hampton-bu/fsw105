f60671d6e7d62fa9cf9d0a2c473fc4078395da01
jesse-hampton-bu <65149406+jesse-hampton-bu@users.noreply.github.com>
2020-07-26 19:27:35 -0700

    sixth-commit

--- week4/calculator.js
+++ week4/calculator.js
@@ -3,29 +3,29 @@ let readlineSync = require('readline-sync');
 
 
 function add (){
-    let num1 = readlineSync.question('enter your first number? ');
-    let num2 = readlineSync.question('enter your second number? '); 
-    console.log(Number(num1) + Number(num2))
+    let num1 = readlineSync.question('please enter your first number? ');
+    let num2 = readlineSync.question('please enter your second number? '); 
+    console.log("the result is" + " " +(Number(num1) + Number(num2)))
 }
 function subtract(){
-    let num1 = readlineSync.question('enter your first number? ');
-    let num2 = readlineSync.question('enter your second number? '); 
-    console.log(Number(num1) - Number(num2))
+    let num1 = readlineSync.question('please enter your first number? ');
+    let num2 = readlineSync.question('please enter your second number? '); 
+    console.log("the result is" + " " +(Number(num1) - Number(num2)))
 }
 function multiply(){
-    let num1 = readlineSync.question('enter your first number? ');
-    let num2 = readlineSync.question('enter your second number? '); 
-    console.log(Number(num1) * Number(num2))
+    let num1 = readlineSync.question('please enter your first number? ');
+    let num2 = readlineSync.question('please enter your second number? '); 
+    console.log("the result is" + " " +(Number(num1) * Number(num2)))
 }
 function divide(){
-    let num1 = readlineSync.question('enter your first number? ');
-    let num2 = readlineSync.question('enter your second number? '); 
-    console.log(Number(num1) / Number(num2))
+    let num1 = readlineSync.question('please enter your first number? ');
+    let num2 = readlineSync.question('please enter your second number? '); 
+    console.log("the result is" + " " +(Number(num1) / Number(num2)))
 }
 
 
 function census() {
-    let sum = readlineSync.question('enter the operation to perform: add, sub, mul, div:')
+    let sum = readlineSync.question('please enter the operation to perform: add, sub, mul, div:')
     if (sum === 'add'){
         add() 
     }else if (sum === 'sub'){
@@ -40,4 +40,7 @@ function census() {
 }
 
 
-census('The result is `${census("add")}')
+census()
+census()
+census()
+census()
